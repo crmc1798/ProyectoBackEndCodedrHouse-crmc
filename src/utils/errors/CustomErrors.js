255afeefc01e5cec378fb65edc7f276ef073e368
crmc1798 <crmc1798@gmail.com>
2023-06-29 17:29:39 -0700

    v0.05 (correccion del mismo error)

--- src/utils/errors/CustomErrors.js
+++ src/utils/errors/CustomErrors.js
@@ -1,4 +1,4 @@
-class customError{
+class CustomError{
     static createError = ({name = "Error", cause, message, code = 1}) => {
         const error = new Error(message);
         error.name = name;
@@ -10,4 +10,4 @@ class customError{
 
 }
 
-module.exports = customError;
+module.exports = CustomError;
